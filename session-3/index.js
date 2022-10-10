a35fd0fa51bb44cbb819b4cd20bc6dc0a948bb1b
Savannah <Savw_77@hotmail.com>
2022-10-10 18:54:19 +0100

    session task 1

--- session-3/index.js
+++ session-3/index.js
@@ -1,2 +1,13 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
+let a =5;
+let b= 10;
+const c = a+b;
+
+console.log(a+b); // outcome is 15
+
+a=20; 
+
+console.log(a+b); //outcome is 30
+
+console.log(c);// outcome is 30
